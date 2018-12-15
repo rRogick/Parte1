af37e813c53dbd5efeca1fb6e9d1b68745cbe08d
Renan Rogick <rRogick@users.noreply.github.com>
2018-12-15 18:53:53 -0200

    Create 02-Operadores.R

---
+++ 02-Operadores.R
@@ -0,0 +1,54 @@
+## Operadores Basicos, Relacionais e Logicos em R
+
+# Operadores Basicos
+
+# Soma
+4 + 4
+
+# Sutracao
+4 - 4
+
+# Multiplicacao
+4 * 4
+
+# Divisao
+4 / 4
+
+# Potencia
+4^2
+4**2
+
+# Modulo
+14 %% 3
+
+
+# Operadores Relacionais
+
+# Atibuindo variaveis
+a = 7
+b = 5
+
+# Operadores
+a > 8
+a < 8
+a <= 8
+a >= 8
+a == 8
+a != 8
+
+# Operadores logicos
+
+# And
+(a==8) & (b==6)
+(a==7) & (b==5)
+(a==8) & (b==5)
+
+# Or
+(a==8) | (b>5)
+(a==8) | (b>=5)
+
+# Not
+a > 8
+print (!a > 8)
+
+
